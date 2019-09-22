86c43eb6c606667338f2802dfa88b70a717c3ad4
Ieva Sinkeviciute <ieva1919@gmail.com>
2019-09-22 14:23:36 +0300

    Trys pulapiai: švieslentė, administravimas ir specialisto

--- administravimo.js
+++ administravimo.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
     $.get("klientai.json", function(data, status){
     alert("Data: " + data + "\nStatus: " + status);
     });
+
+    $("button").click(function(){
+        alert("Jūsų registracija sėkminga!");
+      });
 });
